e15ae81c96799d627118cb5635a84b62847d7646
cydxxzg <zjli@localhost.localdomain>
2026-05-24 14:43:09 -0400

    feat: fix_bash

--- code/test_combine.sh
+++ code/test_combine.sh
@@ -2,7 +2,7 @@ python Compress.py  \
  -name CMV \
  -predictor ../model_zoo/DenseLSTM2_HepG2.pth \
  -generator ../model_zoo/WGAN_HepG2.pth \
- -enhancer_path /home/zjli/Empressor/data/enhancer/cmv_enhancer.txt \
- -motif_split /home/zjli/Empressor/data/enhancer/cmv_enhancer_motif.txt \
+ -enhancer_path ../data/enhancer/cmv_enhancer.txt \
+ -motif_split ../data/enhancer/cmv_enhancer_motif.txt \
  -tag HepG2
 
